463ef4052ab6e5ff8f4ba727ed140b07385509f0
duraipandiyan <113192257+duraipandiyan@users.noreply.github.com>
2022-10-14 21:36:57 +0530

    Update frame.tk_.py

--- frame.tk_.py
+++ frame.tk_.py
@@ -5,11 +5,6 @@ window.geometry("900x400")
 window.resizable(height="false",width="false")
 frame1=Label(window,text="REGESTATION",font=("times",20,"bold"),width=10,pady=30,padx=30,fg="black",bg='white')
 frame1.grid(row=0,column=0)
-<<<<<<< HEAD
-lb_n=Label(frame1,text="Name",font=("times",15,"bold"),pady=20,padx=30,fg="black",bg='white')
-
-=======
-lb_n=Label(frame1,text="Name",font=("times",15,"bold"),pady=10,padx=20,fg="black",bg='white')
+lb_n=Label(frame1,text="Name",font=("times",15,"bold"),pady=20,padx=30,fg="black",bg='white'
 lb_n.grid(row=1,column=0)
->>>>>>> 2d9522b69cb6b2045be41d619e5390e1cfa2d8d8
 window.mainloop()
